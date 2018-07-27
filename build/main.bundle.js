0ffaa9deb0b0093b85c79ec3602b4e0df15c1404
Bernadetta Wąsowska <bernadetta.wasowska@gmail.com>
2018-07-27 08:20:49 +0200

    Remove js folder, change counter class

--- build/main.bundle.js
+++ build/main.bundle.js
@@ -1,116 +1,144 @@
-"use strict";
 'use strict';
 
-var Counter = React.createClass({
-	//funkcja, która zwraca obiekt ze stanem
+var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
 
-	getDefaultProps: function getDefaultProps() {
-		console.log('Initialization phase 1. getDefaultProps()');
-	},
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
 
-	getInitialState: function getInitialState() {
-		console.log('Initialization phase 2. getInitialState()');
-		return {
-			counter: 0
-		};
-	},
+function _possibleConstructorReturn(self, call) { if (!self) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return call && (typeof call === "object" || typeof call === "function") ? call : self; }
 
-	componentWillMount: function componentWillMount() {
-		console.log('Initialization phase 3. componentWillMount()');
-		this.setState({
-			counter: 1000
-		});
-	},
+function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
 
-	render: function render() {
-		var _this = this;
+var Counter = function (_React$Component) {
+    _inherits(Counter, _React$Component);
 
-		//nie działa setState()
-		console.log('Initialization phase 4. render()');
-		return React.createElement('div', {}, React.createElement('button', { className: 'incrementButton', onClick: function onClick(event) {
-				_this.increment();
-			} }, '+'), React.createElement('button', { className: 'decrementButton', onClick: function onClick(event) {
-				_this.decrement();
-			} }, '-'), React.createElement('span', {}, 'Counter:' + this.state.counter));
-	},
+    function Counter() {
+        _classCallCheck(this, Counter);
 
-	componentDidMount: function componentDidMount() {
-		console.log('Initialization phase 5. componentDidMount()');
-		//fetch data
-		//w niej możemy wykonać np. jakieś manipulacje DOM
-	},
+        return _possibleConstructorReturn(this, (Counter.__proto__ || Object.getPrototypeOf(Counter)).apply(this, arguments));
+    }
 
-	componentWillReceiveProps: function componentWillReceiveProps() {
-		console.log('Update phase 1. componentWillReceiveProps()');
-		//stan nie może być aktualizowany przez rodzica komponentu
-	},
+    _createClass(Counter, [{
+        key: 'getDefaultProps',
+        value: function getDefaultProps() {
+            console.log('Initialization phase 1. getDefaultProps()');
+        }
+    }, {
+        key: 'getInitialState',
+        value: function getInitialState() {
+            console.log('Initialization phase 2. getInitialState()');
+            return {
+                counter: 0
+            };
+        }
+    }, {
+        key: 'componentWillMount',
+        value: function componentWillMount() {
+            console.log('Initialization phase 3. componentWillMount()');
+            this.setState({
+                counter: 1000
+            });
+        }
+    }, {
+        key: 'render',
+        value: function render() {
+            var _this2 = this;
 
-	shouldComponentUpdate: function shouldComponentUpdate() {
-		console.log('Update phase 2. shouldComponentUpdate()');
-		if ((this.state.counter + 1) % 5 === 0) {
-			return true;
-		} else {
-			return false;
-		}
-	},
+            //nie działa setState()
+            console.log('Initialization phase 4. render()');
+            return React.createElement('div', {}, React.createElement('button', { className: 'incrementButton', onClick: function onClick(event) {
+                    _this2.increment();
+                } }, '+'), React.createElement('button', { className: 'decrementButton', onClick: function onClick(event) {
+                    _this2.decrement();
+                } }, '-'), React.createElement('span', {}, 'Counter:' + this.state.counter));
+        }
+    }, {
+        key: 'componentDidMount',
+        value: function componentDidMount() {
+            console.log('Initialization phase 5. componentDidMount()');
+            //fetch data
+            //w niej możemy wykonać np. jakieś manipulacje DOM
+        }
+    }, {
+        key: 'componentWillReceiveProps',
+        value: function componentWillReceiveProps() {
+            console.log('Update phase 1. componentWillReceiveProps()');
+            //stan nie może być aktualizowany przez rodzica komponentu
+        }
+    }, {
+        key: 'shouldComponentUpdate',
+        value: function shouldComponentUpdate() {
+            console.log('Update phase 2. shouldComponentUpdate()');
+            if ((this.state.counter + 1) % 5 === 0) {
+                return true;
+            } else {
+                return false;
+            }
+        }
+    }, {
+        key: 'componentWillUpdate',
+        value: function componentWillUpdate() {
+            console.log('Update phase 3. componentWillUpdate()');
+            //nie działa setState()
+        }
+    }, {
+        key: 'componentDidUpdate',
+        value: function componentDidUpdate() {
+            console.log('Update phase 5. componentDidUpdate()');
+        }
+    }, {
+        key: 'componentWillUnmount',
+        value: function componentWillUnmount() {
+            console.log('Delete phase componentWillUnmount()');
+            // clean-up rzeczy związane z odpinaniem timerów czy nasłuchiwania zdarzeń na elementach DOM
+        }
+    }, {
+        key: 'increment',
+        value: function increment() {
+            this.setState({
+                counter: this.state.counter + 1
+            });
+        }
+    }, {
+        key: 'decrement',
+        value: function decrement() {
+            this.setState({
+                counter: this.state.counter - 1
+            });
+        }
+    }]);
 
-	componentWillUpdate: function componentWillUpdate() {
-		console.log('Update phase 3. componentWillUpdate()');
-		//nie działa setState()
-	},
-
-	componentDidUpdate: function componentDidUpdate() {
-		console.log('Update phase 5. componentDidUpdate()');
-	},
-
-	componentWillUnmount: function componentWillUnmount() {
-		console.log('Delete phase componentWillUnmount()');
-		// clean-up rzeczy związane z odpinaniem timerów czy nasłuchiwania zdarzeń na elementach DOM
-	},
-
-	increment: function increment() {
-		this.setState({
-			counter: this.state.counter + 1
-		});
-	},
-
-	decrement: function decrement() {
-		this.setState({
-			counter: this.state.counter - 1
-		});
-	}
-
-});
+    return Counter;
+}(React.Component);
 
 var IndependentCounter = React.createClass({
-	//funkcja, która zwraca obiekt ze stanem
-	getInitialState: function getInitialState() {
-		return {
-			counter: 0
-		};
-	},
+    //funkcja, która zwraca obiekt ze stanem
+    getInitialState: function getInitialState() {
+        return {
+            counter: 0
+        };
+    },
 
-	increment: function increment() {
-		this.setState({
-			counter: this.state.counter + 1
-		});
-	},
+    increment: function increment() {
+        this.setState({
+            counter: this.state.counter + 1
+        });
+    },
 
-	decrement: function decrement() {
-		this.setState({
-			counter: this.state.counter - 1
-		});
-	},
+    decrement: function decrement() {
+        this.setState({
+            counter: this.state.counter - 1
+        });
+    },
 
-	render: function render() {
-		var _this2 = this;
+    render: function render() {
+        var _this3 = this;
 
-		return React.createElement('div', {}, React.createElement('button', { className: 'incrementButton', onClick: function onClick(event) {
-				_this2.increment();
-			} }, '+'), React.createElement('button', { className: 'decrementButton', onClick: function onClick(event) {
-				_this2.decrement();
-			} }, '-'), React.createElement('span', {}, 'IndependentCounter:' + this.state.counter));
-	}
+        return React.createElement('div', {}, React.createElement('button', { className: 'incrementButton', onClick: function onClick(event) {
+                _this3.increment();
+            } }, '+'), React.createElement('button', { className: 'decrementButton', onClick: function onClick(event) {
+                _this3.decrement();
+            } }, '-'), React.createElement('span', {}, 'IndependentCounter:' + this.state.counter));
+    }
 });
 
 var counter = React.createElement('div', {}, React.createElement(Counter), React.createElement(IndependentCounter));
--- modules/Counter.js
+++ modules/Counter.js
@@ -1,114 +1,113 @@
-var Counter = React.createClass({
-	//funkcja, która zwraca obiekt ze stanem
+class Counter extends React.Component {
+    
+    getDefaultProps()  {
+        console.log('Initialization phase 1. getDefaultProps()');
+    }
 
-	getDefaultProps: function()  {
-		console.log('Initialization phase 1. getDefaultProps()');
-	},
+    getInitialState() {
+        console.log('Initialization phase 2. getInitialState()');
+        return {
+            counter: 0
+        };
+    }
 
-	getInitialState: function() {
-		console.log('Initialization phase 2. getInitialState()');
-		return {
-			counter: 0
-		};
-	},
+    componentWillMount() {
+        console.log('Initialization phase 3. componentWillMount()');
+        this.setState({
+            counter: 1000
+        });
+    }
 
-	componentWillMount: function() {
-		console.log('Initialization phase 3. componentWillMount()');
-		this.setState({
-			counter: 1000
-		});
-	},
+    render() {
+        //nie działa setState()
+        console.log('Initialization phase 4. render()');
+        return React.createElement('div', {}, 
+            React.createElement('button', {className: 'incrementButton', onClick: (event) => {this.increment()} }, '+'),
+            React.createElement('button', {className: 'decrementButton', onClick: (event) => {this.decrement()} }, '-'),
+            React.createElement('span', {}, 'Counter:' + this.state.counter)
+        );
+    }
 
-	render: function() {
-		//nie działa setState()
-		console.log('Initialization phase 4. render()');
-		return React.createElement('div', {}, 
-			React.createElement('button', {className: 'incrementButton', onClick: (event) => {this.increment()} }, '+'),
-			React.createElement('button', {className: 'decrementButton', onClick: (event) => {this.decrement()} }, '-'),
-			React.createElement('span', {}, 'Counter:' + this.state.counter)
-		);
-	},
+    componentDidMount() {
+        console.log('Initialization phase 5. componentDidMount()');
+        //fetch data
+        //w niej możemy wykonać np. jakieś manipulacje DOM
+    }
 
-	componentDidMount: function() {
-		console.log('Initialization phase 5. componentDidMount()');
-		//fetch data
-		//w niej możemy wykonać np. jakieś manipulacje DOM
-	},
+    componentWillReceiveProps() {
+        console.log('Update phase 1. componentWillReceiveProps()');
+        //stan nie może być aktualizowany przez rodzica komponentu
+    }
 
-	componentWillReceiveProps: function() {
-		console.log('Update phase 1. componentWillReceiveProps()');
-		//stan nie może być aktualizowany przez rodzica komponentu
-	},
+    shouldComponentUpdate() {
+        console.log('Update phase 2. shouldComponentUpdate()');
+        if( (this.state.counter + 1 ) % 5 === 0) {
+            return true;
+        } else {
+            return false;
+        }
+    }
 
-	shouldComponentUpdate: function() {
-		console.log('Update phase 2. shouldComponentUpdate()');
-		if( (this.state.counter + 1 ) % 5 === 0) {
-			return true;
-		} else {
-			return false;
-		}
-	},
+    componentWillUpdate() {
+        console.log('Update phase 3. componentWillUpdate()');
+        //nie działa setState()
+    }
 
-	componentWillUpdate: function() {
-		console.log('Update phase 3. componentWillUpdate()');
-		//nie działa setState()
-	},
+    componentDidUpdate() {
+        console.log('Update phase 5. componentDidUpdate()');
+    }
 
-	componentDidUpdate: function() {
-		console.log('Update phase 5. componentDidUpdate()');
-	},
+    componentWillUnmount() {
+        console.log('Delete phase componentWillUnmount()');
+        // clean-up rzeczy związane z odpinaniem timerów czy nasłuchiwania zdarzeń na elementach DOM
+    }
 
-	componentWillUnmount: function() {
-		console.log('Delete phase componentWillUnmount()');
-		// clean-up rzeczy związane z odpinaniem timerów czy nasłuchiwania zdarzeń na elementach DOM
-	},
+    increment() {
+        this.setState({
+            counter: this.state.counter + 1
+        });
+    }
 
-	increment: function() {
-		this.setState({
-			counter: this.state.counter + 1
-		});
-	},
+    decrement() {
+        this.setState({
+            counter: this.state.counter - 1 
+        });
+    }
 
-	decrement: function() {
-		this.setState({
-			counter: this.state.counter - 1 
-		});
-	},
-
-});
+}
 
 var IndependentCounter = React.createClass({
-	//funkcja, która zwraca obiekt ze stanem
-	getInitialState: function() {
-		return {
-			counter: 0
-		};
-	},
+    //funkcja, która zwraca obiekt ze stanem
+    getInitialState: function() {
+        return {
+            counter: 0
+        };
+    },
 
-	increment: function() {
-		this.setState({
-			counter: this.state.counter + 1
-		});
-	},
+    increment: function() {
+        this.setState({
+            counter: this.state.counter + 1
+        });
+    },
 
-	decrement: function() {
-		this.setState({
-			counter: this.state.counter - 1 
-		});
-	},
+    decrement: function() {
+        this.setState({
+            counter: this.state.counter - 1 
+        });
+    },
 
-	render: function() {
-		return React.createElement('div', {}, 
-			React.createElement('button', {className: 'incrementButton', onClick: (event) => {this.increment()} }, '+'),
-			React.createElement('button', {className: 'decrementButton', onClick: (event) => {this.decrement()} }, '-'),
-			React.createElement('span', {}, 'IndependentCounter:' + this.state.counter)
-		);
-	}
+    render: function() {
+        return React.createElement('div', {}, 
+            React.createElement('button', {className: 'incrementButton', onClick: (event) => {this.increment()} }, '+'),
+            React.createElement('button', {className: 'decrementButton', onClick: (event) => {this.decrement()} }, '-'),
+            React.createElement('span', {}, 'IndependentCounter:' + this.state.counter)
+        );
+    }
 });
 
 var counter = React.createElement('div', {},
-	React.createElement(Counter),
-	React.createElement(IndependentCounter)
+    React.createElement(Counter),
+    React.createElement(IndependentCounter)
 );
 
 ReactDOM.render(counter, document.getElementById('app')); 
